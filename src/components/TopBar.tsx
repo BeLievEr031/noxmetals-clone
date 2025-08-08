8209beb11029c723a4e8a2f9a55f54b9650b3d27
BeLieVer031 <sandyrajak031@gmail.com>
2025-08-08 13:22:42 +0530

    fix: reponsivness

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -3,9 +3,9 @@ function TopBar() {
     return (
         <div className="bg-zinc-900">
             <div className="flex-1 flex flex-col items-center justify-center px-8 py-2 text-white">
-                <div className="flex flex-row justify-between items-center w-full max-w-7xl mb-4">
+                <div className="flex flex-col md:flex-row justify-center md:justify-between items-center w-full max-w-7xl mb-4">
                     <div>
-                        <h1 className="text-3xl font-semibold mt-2 mb-4 text-white text-left">
+                        <h1 className="text-3xl text-center md:text-left font-semibold mt-2 mb-4 text-white">
                             NOX METALS
                         </h1>
                         <p className="mb-4 text-center text-xl">
@@ -14,7 +14,7 @@ function TopBar() {
                     </div>
                     <button
                         data-slot="button"
-                        className="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive border shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50 h-9 px-4 py-2 has-[>svg]:px-3 text-white bg-transparent mt-0"
+                        className="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all border cursor-pointer h-9 px-4 py-2 has-[>svg]:px-3 text-white bg-transparent mt-0 hover:bg-primary-foreground hover:text-primary"
                     >
                         Join Mailing Wishlist
                     </button>
